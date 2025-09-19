55f94b2d6b2f13cae67f2d7d1971f5c42a7c2d71
KoungQ <guu119@naver.com>
2025-09-19 14:54:25 +0900

    fix: use H2 database for tests instead of MySQL

--- src/test/java/com/project/user/controller/UserControllerTest.java
+++ src/test/java/com/project/user/controller/UserControllerTest.java
@@ -16,7 +16,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureWebMvc
-@ActiveProfiles("test-mysql")
+@ActiveProfiles("test")
 class UserControllerTest {
 
     @Autowired
