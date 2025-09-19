2637bc4f68791be8798172b3fe3d44292160b5b8
KoungQ <guu119@naver.com>
2025-09-19 16:37:47 +0900

    test

--- src/main/java/com/project/user/domain/ui/LoginController.java
+++ src/main/java/com/project/user/domain/ui/LoginController.java
@@ -1,15 +1,15 @@
-//package com.project.user.domain.ui;
-//
-//import com.project.user.domain.application.dto.request.LoginRequest;
-//import com.project.user.domain.application.dto.response.LoginResponse;
-//import com.project.user.domain.application.usecase.LoginUseCase;
-//import com.project.user.domain.ui.spec.LoginApiSpec;
-//import com.project.user.global.common.BaseResponse;
-//import jakarta.validation.Valid;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.RestController;
-//
+package com.project.user.domain.ui;
+
+import com.project.user.domain.application.dto.request.LoginRequest;
+import com.project.user.domain.application.dto.response.LoginResponse;
+import com.project.user.domain.application.usecase.LoginUseCase;
+import com.project.user.domain.ui.spec.LoginApiSpec;
+import com.project.user.global.common.BaseResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
 //@RestController
 //@RequiredArgsConstructor
 //public class LoginController implements LoginApiSpec {
