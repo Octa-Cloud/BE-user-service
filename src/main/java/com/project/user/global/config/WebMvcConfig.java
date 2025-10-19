2ac63f34790e82ad26445835379957bf3cc1a169
KoungQ <guu119@naver.com>
2025-10-19 09:56:04 +0900

    feat: cors

--- src/main/java/com/project/user/global/config/WebMvcConfig.java
+++ src/main/java/com/project/user/global/config/WebMvcConfig.java
@@ -6,6 +6,7 @@ import com.project.user.global.security.TokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -22,5 +23,37 @@ public class WebMvcConfig implements WebMvcConfigurer {
                 new CurrentUserArgumentResolver(tokenProvider),
                 new AccessTokenArgumentResolver(tokenProvider)
         ));
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins(
+                        "http://localhost:5173",
+                        "https://www.mong.live",
+                        "http://www.mong.live"
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH", "HEAD")
+                .allowedHeaders(
+                        "Authorization",
+                        "Content-Type",
+                        "X-Requested-With",
+                        "Accept",
+                        "Origin",
+                        "Access-Control-Request-Method",
+                        "Access-Control-Request-Headers",
+                        "X-User-Id",
+                        "Cache-Control",
+                        "Pragma"
+                )
+                .exposedHeaders(
+                        "Authorization",
+                        "Content-Type",
+                        "X-User-Id",
+                        "Access-Control-Allow-Origin",
+                        "Access-Control-Allow-Credentials"
+                )
+                .allowCredentials(true)
+                .maxAge(3600);
     }
 }
